13324d11fca07c70c82cd789f256cb247490a64e
sudhirj290416 <83187763+sudhirj290416@users.noreply.github.com>
2021-09-15 09:42:58 -0400

    Update hello.js

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -1,7 +1,8 @@
 async function hello (event, context){
   for (const {body} of event.Records) {
     console.log(`Sqs Message: ${body}`);
-	console.log('adding new message');
+    console.log('adding new message');
+    console.log('adding new message again');
   }
 };
 
